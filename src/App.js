21967b0877fe3e433ff8fe18c31d8eaa144de294
shafi <116013717+myselfShafi@users.noreply.github.com>
2024-09-01 23:34:10 +0530

    change to cards list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
 import CountDownTimer from "./components/countdown-timer";
 
+let topics = [
+  { title: "Form Validation", component: <></> },
+  { title: "Countdown Timer", component: <CountDownTimer /> },
+  { title: "Data Pagination", component: <></> },
+];
+
 function App() {
   return (
     <div className="App">
-      <CountDownTimer />
+      {topics.map((list) => (
+        <button
+          className="card"
+          key={list.title}
+          onClick={() => console.log(list.title)}
+        >
+          <h2>{list.title}</h2>
+        </button>
+      ))}
     </div>
   );
 }
